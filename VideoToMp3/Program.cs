63579c927a582e99337b5ae2bf8869f5e3045660
apollo78124 <98apollo@naver.com>
2022-04-12 23:21:54 -0700

    Project init

---
+++ VideoToMp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace VideoToMp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+        }
+    }
+}
